ccab0182b9ae8bd56ae0bfaf8e2cf8d497c48275
yoshi49535 <yoshi+github@1o1.co.jp>
2026-04-02 22:54:36 +0900

    chore(core): replace node-config with ts.hocon + zod dependencies

--- packages/core/vitest.config.mjs
+++ packages/core/vitest.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["src/**/*.spec.mts"],
+    include: ["src/**/*.spec.mts", "src/**/*.test.mts"],
     environment: "node",
     globals: true,
   },
